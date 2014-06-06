0120ea3f104bbaf451c88f8e9b3273e8a5a2f07b
lipemat <mat@matlipe.com>
2014-06-05 20:10:01 -0500

    basic structure up

--- index.php
+++ index.php
@@ -1,6 +1,17 @@
-<h1>Hello World</h1>
-
-
-<p><em> Testing sush and pull functions</em></p>
-
-<img src="http://media.trafficjunky.net/ads/005/653/531/image5653531.gif" />
+<html>
+	<head>
+		<link rel="resources/css/style.css" type="text/css" >
+		<script src="resources/js/default.js"></script>
+	</head>
+	<body>
+		<header>
+			<h1>FU</h1>
+		</header>
+		<article>
+			<img src="resources/img/image5653531.gif" />
+		</article>
+		<footer>
+			<a href="http://pornhub.com">Made by me</a>
+		</footer>
+		
+	</body>
