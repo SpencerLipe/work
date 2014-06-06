3c4547140647892fac5b3311ef80dd9ac0fd31ef
lipemat <mat@matlipe.com>
2014-06-05 20:27:52 -0500

    go

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <html>
 	<head>
 		<link rel="resources/css/style.css" type="text/css" >
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 		<script src="resources/js/default.js"></script>
 	</head>
 	<body>
