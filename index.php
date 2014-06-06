4cc34836ef8842c7d0f442e75d2e7a6f0815cd63
lipemat <mat@matlipe.com>
2014-06-05 20:43:39 -0500

    fixed the stylesheet

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <html>
 	<head>
-		<link rel="resources/css/style.css" type="text/css" >
+		<link rel='stylesheet' href="resources/css/style.css" type="text/css" >
 		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 		<script src="resources/js/default.js"></script>
 	</head>
